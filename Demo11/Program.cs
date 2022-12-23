a793f85b770a6e7348259dbd311cde1e5d012a4b
Soumya K <ksoumya34@gmail.com>
2022-12-23 14:49:58 +0530

    test'

---
+++ Demo11/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Demo11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a driver instance for chromedriver
+            IWebDriver driver = new ChromeDriver();
+            
+
+            //Navigate to google page
+            driver.Navigate().GoToUrl("http:www.google.com");
+
+            //Maximize the window
+            driver.Manage().Window.Maximize();
+
+            //Find the Search text box using xpath
+            IWebElement element = driver.FindElement(By.XPath("//*[@title='Search']"));
+
+           //Enter some text in search text box
+            element.SendKeys("learn-automation");
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//a[text()='Gmail']")).Click();
+
+            //Close the browser
+            // driver.Close();
+
+        }
+    }
+}
